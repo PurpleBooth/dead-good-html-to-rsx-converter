eb34cd5ad5ac76459844137f6d6a5ca028f36d37
Billie Thompson <billie@billiecodes.com>
2023-10-28 22:35:59 +0200

    feat: Derive diagnostic for errors

--- src/errors.rs
+++ src/errors.rs
@@ -1,8 +1,9 @@
+use miette::Diagnostic;
 use thiserror::Error;
 
 pub type Result<T> = std::result::Result<T, Error>;
 
-#[derive(Error, Debug)]
+#[derive(Error, Debug, Diagnostic)]
 pub enum Error {
     #[error("failed to parse html")]
     HtmlParseError(#[from] tl::errors::ParseError),
